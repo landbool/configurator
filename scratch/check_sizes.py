df5c71cb7970162383fb9d18a5db0e2909131289
landbool <ivaxa26@mail.ru>
2026-09-10 01:19:11 +0300

    docs(log): update PROJECT_LOG.md for GFHF39-159 and clean scratch files from repo

--- scratch/check_sizes.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as ET
-
-svg_data = open('scratch/GFHF39-89_numbered.svg', 'r', encoding='utf-8').read()
-root = ET.fromstring(svg_data)
-for el in root.iter():
-    tag = el.tag.split('}')[-1]
-    if tag == 'rect' and el.attrib.get('fill') == 'white':
-        print(f"w={el.attrib['width']}, h={el.attrib['height']}")
